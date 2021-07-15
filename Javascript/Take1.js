7cf5fb35f37e0f56f76b9ce5f64aba51a04f8cb5
Nirand <56184504+nnnpooh@users.noreply.github.com>
2021-07-15 17:32:29 +0700

    javascript tutorial

---
+++ Javascript/Take1.js
@@ -0,0 +1,111 @@
+/* 
+! Variable declaration
+ */
+
+// var a = 1;
+// var b = 2;
+// console.log(a + b);
+// let c = 1;
+// const d = 1;
+
+/* 
+! Data Type
+ */
+// var a = 1;
+// console.log(typeof a);
+// var b = 'Hello World';
+// console.log(b)
+// console.log(typeof(b))
+// var c = true
+// var c = false
+// console.log(c)
+// console.log(typeof(c))
+
+/* 
+! Data type manipulation
+ */
+// var a = 'Hello'
+// var b = 'World'
+// console.log(a + ' ' + b + '!')
+// var c = 10
+// console.log(a + c)
+// var cString = c.toString()
+// console.log(typeof(cString))
+// var e = 'Hello'.toUpperCase()
+// console.log(e)
+
+/* 
+! Collection
+? Array
+ */
+// var a = [1,2,3,4,5,6,7,'Hello','World',true,false]
+// console.log(a)
+// console.log(a[0])
+// console.log(a[1])
+// console.log(a[7])
+// console.log(a.slice(0,4))
+
+/* 
+! Collection
+? Object (Python dictionary)
+ */
+
+// var a = {
+//   number: 10,
+//   title: 'Hello World',
+//   bool: false,
+//   number2: 15,
+//   'this is-key' : 'this is the value'
+// };
+
+// console.log(a.number)
+// console.log(a.title)
+// console.log(a.bool)
+// console.log(a.number2)
+// console.log(a['number'])
+// console.log(a['this is-key'])
+
+/* 
+! Condition
+? If Loop
+ */
+// var a = 25;
+// var b = null;
+// if (a < 10) {
+//   b = 'Less than 10';
+// } else if (a < 20) {
+//   b = 'Between 10 and 20';
+// } else {
+//   b = 'Greater than 20';
+// }
+// console.log(b);
+
+/* 
+! Condition
+? Ternary operator
+ */
+// var a = 25;
+// var b = null;
+// if (a <= 10) {
+//   b = 'Less than 10';
+// } else {
+//   b = 'Greater than 10';
+// }
+
+// var c = a <= 10 ? 'Less than 10' : 'Greater than 10';
+// console.log(c);
+
+/* 
+! For Loop
+ */
+// var a = null;
+// for (var i = 0; i < 10; i++) {
+//   a = i * 5;
+//   console.log(a);
+// }
+
+// var a = [];
+// for (var i = 0; i < 10; i++) {
+//   a.push(i);
+// }
+// console.log(a);
