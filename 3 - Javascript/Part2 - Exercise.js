a40163b79bec763e847b8ab15a4b60708d98f44f
Nirand <56184504+nnnpooh@users.noreply.github.com>
2021-07-22 15:40:54 +0700

    update javascript

---
+++ 3 - Javascript/Part2 - Exercise.js
@@ -0,0 +1,70 @@
+const data = [
+  {
+    id: 1,
+    first_name: 'Berky',
+    city: 'United States',
+    age: 23,
+  },
+  {
+    id: 2,
+    first_name: 'Renado',
+    city: 'United States',
+    age: 42,
+  },
+  {
+    id: 3,
+    first_name: 'Amalie',
+    city: 'Thailand',
+    age: 25,
+  },
+  {
+    id: 4,
+    first_name: 'Phaidra',
+    city: 'Thailand',
+    age: 25,
+  },
+  {
+    id: 5,
+    first_name: 'Tess',
+    city: 'United States',
+    age: 38,
+  },
+  {
+    id: 6,
+    first_name: 'Rosaline',
+    city: 'United States',
+    age: 23,
+  },
+  {
+    id: 7,
+    first_name: 'Cyndia',
+    city: 'Thailand',
+    age: 54,
+  },
+  {
+    id: 8,
+    first_name: 'Bil',
+    city: 'United States',
+    age: 25,
+  },
+  {
+    id: 9,
+    first_name: 'Ondrea',
+    city: 'Thailand',
+    age: 41,
+  },
+  {
+    id: 10,
+    first_name: 'Alicia',
+    city: 'United States',
+    age: 30,
+  },
+];
+
+// Print ข้อมูล ชื่อเท่านั้น
+
+// เอาคนที่ไม่ใช้ Thailand ออก
+
+// ทำให้ ชื่อเป็นตัวเล็ก
+
+// รวมอายุทุกคน
---
+++ 3 - Javascript/Part2.js
@@ -0,0 +1,89 @@
+/* 
+! Function
+*/
+
+/* 
+? Normal function (with and without return)
+*/
+
+/* function helloWorld() {
+  console.log('Hello');
+}
+helloWorld();
+
+function hello(a, b) {
+  console.log([...arguments]);
+}
+hello('nirand', 'pooh');
+*/
+
+/* 
+? Arrow function
+*/
+
+/* const a = () => console.log('Hello');
+a();
+
+//const sumNum = (b, c) => b + c;
+const sumNum = (b, c) => {
+  return b + c;
+};
+console.log(sumNum(1, 2));
+ */
+
+/* 
+? Higher order function
+*/
+
+// const isEven = (n) => {
+//   return n % 2 == 0;
+// };
+
+// let printMsg = (evenFunc, num) => {
+//   const isNumEven = evenFunc(num);
+//   console.log(`The number ${num} is an even number: ${isNumEven}.`);
+// };
+// printMsg(isEven, 4);
+
+/* 
+! Array method
+*/
+/* 
+? ForEach
+*/
+// const numbers = [28, 77, 45, 99, 27];
+
+// numbers.forEach((number) => {
+//   console.log(number);
+// });
+/* 
+? Map
+*/
+
+// const finalParticipants = ['Taylor', 'Donald', 'Don', 'Natasha', 'Bobby'];
+
+// const announcements = finalParticipants.map((member) => {
+//   return member + ' joined the contest.';
+// });
+
+// console.log(announcements);
+/* 
+? Filter
+*/
+// const randomNumbers = [4, 11, 42, 14, 39];
+// const filteredArray = randomNumbers.filter((n) => {
+//   return n > 5;
+// });
+/* 
+
+/* 
+? Reduce
+*/
+
+// const arrayOfNumbers = [1, 2, 3, 4];
+
+// const sum = arrayOfNumbers.reduce((accumulator, currentValue) => {
+//   return accumulator + currentValue;
+// });
+
+// console.log(sum); // 10
