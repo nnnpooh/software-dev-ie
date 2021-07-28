30e21cb95cdf1b1ed5931051012c07976d24f80d
nnnpooh <nnnpooh@gmail.com>
2021-07-28 17:03:13 +0700

    update dom

--- 4 - DOM/index.js
+++ 4 - DOM/index.js
@@ -1,12 +1,19 @@
-window.onload = (event) => {
+window.onload = () => {
   console.log('page is fully loaded');
-  console.log(event);
 };
 
+window.console.dir(window);
+console.dir(window.document);
+console.dir(document);
+
+// Inject content
 document.getElementById('p1').innerHTML = 'My First JavaScript';
 
+// Create element
 var para = document.createElement('p');
 para.innerHTML = 'This is from javascript';
+
+//
 var element = document.getElementById('div1');
 element.appendChild(para);
 
