ca60a6af41f702aed8048314513acd5e9da7dc5c
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2021-09-15 16:14:57 +0700

    update react-firebase

--- c9-react-firebase/src/App.js
+++ c9-react-firebase/src/App.js
@@ -1,22 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import db from './db';
 
-function clickFn1() {
-  db.collection('users')
-    .get()
-    .then((snapshot) => {
-      snapshot.docs.forEach((doc) => {
-        console.log(doc.data());
+function onSnapShotFunction(snapshot, setTodos) {
+  let changes = snapshot.docChanges();
+  changes.forEach((change) => {
+    //console.log(change.type);
+    //console.log(change.doc.data());
+    let data = change.doc.data();
+    const id = change.doc.id;
+    data.id = id;
+    if (change.type === 'added') {
+      setTodos((prev) => [...prev, data]);
+    } else if (change.type === 'removed') {
+      setTodos((prev) => prev.filter((el) => (el.id !== id ? true : false)));
+    } else if (change.type === 'modified') {
+      setTodos((prev) => {
+        const dataFiltered = prev.filter((el) => (el.id !== id ? true : false));
+        return [...dataFiltered, data];
       });
-    });
+    }
+  });
 }
 
-console.log(db);
-
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = db
+      .collection('users')
+      .onSnapshot((snapshot) => onSnapShotFunction(snapshot, setTodos));
+    return () => unsubscribe();
+  }, []);
+
   return (
     <div className='App'>
-      <button onClick={clickFn1}>Click Me</button>
+      <h1>Todos</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
